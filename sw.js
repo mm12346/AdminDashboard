8d7eb2752c8e943195d1cb940ccddfb937da9efb
mm12346 <xoctell338@gmail.com>
2025-06-18 19:48:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ชื่อของ Cache Storage ที่จะใช้เก็บไฟล์
-const CACHE_NAME = 'online-checker-cache-v1';
+const CACHE_NAME = 'online-checker-cache-v1.1';
 
 // รายการไฟล์ที่ต้องการให้ถูกแคชไว้เพื่อให้ทำงานออฟไลน์ได้
 const urlsToCache = [
