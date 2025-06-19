7677b936b2477ce6a97f7e71ad1f36690ac59e43
mm12346 <xoctell338@gmail.com>
2025-06-19 10:17:04 +0700

    Update admin-sw.js

--- admin-sw.js
+++ admin-sw.js
@@ -1,5 +1,5 @@
 // ชื่อของ Cache Storage ที่จะใช้เก็บไฟล์สำหรับหน้าแอดมิน
-const CACHE_NAME = 'admin-checker-cache-v1';
+const CACHE_NAME = 'admin-checker-cache-v2';
 
 // รายการไฟล์ที่ต้องการให้ถูกแคชไว้
 const urlsToCache = [
